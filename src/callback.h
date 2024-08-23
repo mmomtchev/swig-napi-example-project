46c383982de95ff0b14bf877d28fdf03f4e53f7b
Momtchil Momtchev <momtchil@momtchev.com>
2024-08-23 14:12:47 +0200

    switch to the new eslint config file (#123)

--- src/callback.h
+++ src/callback.h
@@ -1,5 +1,5 @@
-#include <string>
 #include <functional>
+#include <string>
 
 std::string GiveMeFive(std::function<std::string(int, const std::string &)> giver);
 std::string GiveMeFive_C(std::string (*giver)(void *, int, const std::string &), void *context);
