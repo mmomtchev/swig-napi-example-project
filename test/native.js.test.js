0cae7cf08e3b0c04d801fc6c985b4da5049e2c21
Momtchil Momtchev <momtchil@momtchev.com>
2024-10-08 16:23:03 +0200

    fix an errenous comment

--- test/native.js.test.js
+++ test/native.js.test.js
@@ -1,9 +1,8 @@
 import native from '../lib/native.cjs';
 import { assert } from 'chai';
 
-// This test can be run either in Node.js or in the browser
+// This test is exclusive to Node.js
 // npx run test:nodejs
-// npx run test:browser
 
 describe('native', () => {
   it('can be imported from JS', () => {
