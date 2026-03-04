85532da5e7ca7874fcf3f36015f7375b7deac551
Momtchil Momtchev <momtchil@momtchev.com>
2026-03-04 15:36:29 +0100

    update the comment

--- test/browser/webpack.config.js
+++ test/browser/webpack.config.js
@@ -29,7 +29,7 @@ export default [
      * to not expand those statements - we know that these sections won't be executed in
      * the browser since they are dependant on the environment auto-detection.
      * 
-     * The list is current for emscripten 3.1.51. Later versions may additional
+     * The list is current for emscripten 5.0.2. Later versions may additional
      * symbols.
      * 
      * Alternatively, if you want to publish a WASM that works without any custom
